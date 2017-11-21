118581257acdf7902998c90d254221ef164caae3
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2017-11-21 11:04:36 +0100

    Update effects_helper and input_file

--- effects_helper.py
+++ effects_helper.py
@@ -4,6 +4,7 @@ import random
 import image_helper
 from Effects import surrounding_text_effect, back_page_text_effect
 
+
 def apply_effects(img, text, words_dict, background, config):
     img = surrounding_text_effect.apply_effect(img, text, words_dict, config)
 
@@ -11,7 +12,7 @@ def apply_effects(img, text, words_dict, background, config):
         back_text = back_page_text_effect.generate_back_image(img.shape[1], img.shape[0], text, words_dict, config)
 
         alpha_coef = random.uniform(config['BackText']['minalpha'], config['BackText']['maxalpha'])
-        background = image_helper.place_text_on_background(back_text, background, 0.2)
-       
+        background = image_helper.place_text_on_background(back_text, background, alpha_coef)
+
     img = image_helper.place_text_on_background(img, background)
     return img
