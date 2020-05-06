6bce0785b60fb982924d7a46da541c1fb1e2877c
Tomáš Lessy <lessy.mot@gmail.com>
2020-05-06 20:23:40 +0200

    image_helper more pep-like, sane random texture selection

--- helpers/image_helper.py
+++ helpers/image_helper.py
@@ -1,57 +1,58 @@
 from __future__ import print_function
 import numpy as np
 import cv2
-import os
 import random
+
 
 def place_text_on_background(text_image, background, text_alpha_coef=0.7):
     text_image_height, text_image_width, _ = text_image.shape
-    
+
     result = np.copy(background)
-    result = get_random_part_of_texture(text_image_width, text_image_height, result)
+    result = get_random_part_of_texture(
+        text_image_width, text_image_height, result)
 
     alpha_text = text_image[:, :, 3] / 255.0
     alpha_text = alpha_text * text_alpha_coef
     alpha_back = 1.0 - alpha_text
-    
+
     for c in range(0, 3):
-        result[:text_image_height, :text_image_width, c] = (alpha_text * text_image[:text_image_height, :text_image_width, c] + 
-                                                            alpha_back * result[:text_image_height, :text_image_width, c])
+        result[:text_image_height, :text_image_width, c] = (
+            alpha_text * text_image[:text_image_height, :text_image_width, c] +
+            alpha_back * result[:text_image_height, :text_image_width, c])
 
     result = result[:text_image_height, :text_image_width, :]
-    
+
     return result
 
 
-def get_random_part_of_texture(width, height, texture):
+def get_random_part_of_texture(
+        width: int, height: int, texture: np.array) -> np.array:
     texture_height, texture_width, _ = texture.shape
-    
-    x = random.randint(0, texture_width)
-    y = random.randint(0, texture_height)
 
-    if x + width > texture_width:
-        x = texture_width - width
+    pos_height_range = texture_height - width
+    pos_width_range = texture_width - width
 
-    if y + height > texture_height:
-        y = texture_height - height
+    if pos_height_range < 0 or pos_width_range < 0:
+        raise ValueError("Background is not big enough.",
+                         "Texture shape: " + str(texture.shape),
+                         "Text image shape: " + str((height, width)))
 
-    if x < 0 or y < 0:
-        raise ValueError("Background is not big enough.", "Texture shape: " + str(texture.shape), "Text image shape: " + str((height, width)))
+    x = random.randint(0, pos_width_range)
+    y = random.randint(0, pos_height_range)
 
-    result = np.copy(texture[y:y+height, x:x+width])
-    
-    return result
-    
+    return np.copy(texture[y:y+height, x:x+width])
 
-def draw_annotations(img, annotations, baselines, a_color=[255,0,0], b_color=[0,255,0]):
+
+def draw_annotations(img, annotations, baselines, a_color=[255, 0, 0],
+                     b_color=[0, 255, 0]):
     result = np.copy(img)
 
     for annotation in annotations:
         x, y, w, h = map(int, annotation[1])
-        result[y:y+h, x] = a_color # left edge
-        result[y:y+h, x+w] = a_color # right edge
-        result[y, x:x+w] = a_color # top edge
-        result[y+h, x:x+w] = a_color # bottom edge
+        result[y:y+h, x] = a_color  # left edge
+        result[y:y+h, x+w] = a_color  # right edge
+        result[y, x:x+w] = a_color  # top edge
+        result[y+h, x:x+w] = a_color  # bottom edge
 
     for baseline in baselines:
         x, y, w = map(int, baseline)
@@ -62,14 +63,18 @@ def draw_annotations(img, annotations, baselines, a_color=[255,0,0], b_color=[0,
 
     return result
 
-def add_padding_to_img(img, padding_top=20, padding_bottom=20, padding_left=20, padding_right=20, value=[255,255,255,0]):
-    return cv2.copyMakeBorder(img, 
-                              padding_top, 
-                              padding_bottom, 
-                              padding_left, 
-                              padding_right, 
-                              cv2.BORDER_CONSTANT, 
+
+def add_padding_to_img(img, padding_top=20, padding_bottom=20,
+                       padding_left=20, padding_right=20,
+                       value=[255, 255, 255, 0]):
+    return cv2.copyMakeBorder(img,
+                              padding_top,
+                              padding_bottom,
+                              padding_left,
+                              padding_right,
+                              cv2.BORDER_CONSTANT,
                               value=value)
+
 
 def add_random_padding(back_text, config):
     default_padding = config['Page']['padding']
@@ -81,6 +86,7 @@ def add_random_padding(back_text, config):
                               padding_bottom=2*default_padding-top,
                               padding_left=left,
                               padding_right=2*default_padding-left)
+
 
 def fit_to_dimensions(img, shape):
     target_height, target_width = shape
