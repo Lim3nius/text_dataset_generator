afbd2850b116b70e425ffe3f857f95fcddc948e0
Tomáš Lessy <lessy.mot@gmail.com>
2020-06-04 23:11:40 +0200

    [Fix] generator: PEP8 compliant code

--- text_dataset_generator.py
+++ text_dataset_generator.py
@@ -14,15 +14,16 @@ from helpers import image_helper
 from helpers import effects_helper
 from helpers import text_renderer
 from helpers import xml_helper
-from helpers import semantic_segmentation_helper
+from helpers import semantic_segmentation_helper as sem_seg_help
 from helpers.config_helper import parse_configuration
 from helpers.input_modifications import modify_content
 from helpers.postprocess_helper import background_thresholding
-from helpers import manifest_helper
+# from helpers import manifest_helper
 from helpers import color_helper
 from helpers.dict_writer import SyncWriterWrapper
 from helpers import baseline_helper
 from helpers import logger
+from helpers import layout
 
 log = getLogger()
 
@@ -67,13 +68,13 @@ def parse_arguments():
     parser.add_argument('-c', '--count', type=int, default=10**6,
                         help='Number of images to generate')
     parser.add_argument('-w', '--workers', default=1, type=int,
-                        help="Number of paralell workers to start")
-    parser.add_argument("-l", "--log-level", default='info', type=str,
+                        help='Number of paralell workers to start')
+    parser.add_argument('-l', '--log-level', default='info', type=str,
                         choices=['debug', 'info', 'warning', 'error'],
-                        help="Level of logging")
+                        help='Level of logging')
     parser.add_argument('--prefix', default=None, type=str,
-                        help=("Prefix for newly generated images."
-                              "Config has higer priority"))
+                        help=('Prefix for newly generated images.'
+                              'Config has higer priority'))
 
     parser.add_argument('--font', type=str, default=None,
                         help='Specific font to be used')
@@ -112,7 +113,8 @@ def generator(config, content, index, fonts, backgrounds, args,
             config["Page"]["minlineheight"], config["Page"]["maxlineheight"])
 
         try:
-            text_img, annotations, baselines, new_content = text_renderer.render_page(face, content, config)
+            tmp = text_renderer.render_page(face, content, config)
+            text_img, annotations, baselines, new_content = tmp
             config['Baseline'] = {'text': baselines}
         except Exception:
             traceback.print_exc()
@@ -124,13 +126,11 @@ def generator(config, content, index, fonts, backgrounds, args,
                 text_generation_failures = 0
                 continue
 
-            print("---", file=sys.stderr)
-            print("There was an error during creating image number",
-                  index, file=sys.stderr)
-            print("Text:", content[0][:30], "...", file=sys.stderr)
-            print("Font:", font_name, file=sys.stderr)
-            print("Trying to generate same text again.", file=sys.stderr)
-            print("---", file=sys.stderr)
+            log.warn(
+                f'There was an error during creating image number {index}\n'
+                f'Text: {content[0][:30]}\n'
+                f'Font: {font_name}\n'
+                'Trying to generate same text again.')
             text_generation_failures += 1
             continue
 
@@ -145,19 +145,22 @@ def generator(config, content, index, fonts, backgrounds, args,
             padding_left=config['Padding']['left'],
             padding_right=config['Padding']['right'])
 
-        annotations = update_annotations(annotations, config['Padding']['left'], config['Padding']['top'])
-        baselines = update_baselines(baselines, config['Padding']['left'], config['Padding']['top'])
+        annotations = update_annotations(
+            annotations, config['Padding']['left'], config['Padding']['top'])
+        baselines = update_baselines(
+            baselines, config['Padding']['left'], config['Padding']['top'])
 
         semantic_segmentation_image = None
         image_name = prefix + "_" + str(index)
 
         if config['Common']['semanticsegmentation']:
-            semantic_segmentation_image = semantic_segmentation_helper.generate(text_img, annotations)
+            semantic_segmentation_image = sem_seg_help.generate(
+                text_img, annotations)
 
         if config['Common']['textgroundtruth']:
             segmented = background_thresholding(text_img)
             file_helper.write_image(segmented, config['Common']['outputs'] +
-                                     image_name + '_no_effect.png')
+                                    image_name + '_no_effect.png')
             manifest_row['textgroundtruth'] = image_name + '_no_effect.png'
 
         try:
@@ -165,14 +168,14 @@ def generator(config, content, index, fonts, backgrounds, args,
                                                   config)
         except Exception:
             traceback.print_exc()
-            print("---", file=sys.stderr)
-            print("There was an error during applying effects on image number", (index), file=sys.stderr)
-            print("Text:", content[0][:30], "...", file=sys.stderr)
-            print("Font:", font_name, file=sys.stderr)
-            print("Trying to generate same text again.", file=sys.stderr)
-            print("---", file=sys.stderr)
-
-            print('Skipping 10 characters. Reason: Unable to generate paragraph')
+            log.warn(
+                'There was an error during applying effects'
+                f'on image number {index}\n'
+                f'Text: {content[0][:30]}\n'
+                f'Font: {font_name}\n'
+                f'Trying to generate same text again.\n'
+                f'Skipping 10 characters. Reason: Unable to generate paragraph'
+            )
             content[0] = content[0][10:]
             continue
 
@@ -185,21 +188,27 @@ def generator(config, content, index, fonts, backgrounds, args,
         # image_names.append(image_name + ".png")
         # annotation_names.append(image_name + ".xml")
 
-        transkribus = xml_helper.annotations_and_baselines_to_transkribus_xml(annotations, baselines, image_name + ".png", result.shape[:2])
+        transkribus = xml_helper.annotations_and_baselines_to_transkribus_xml(
+            annotations, baselines, image_name + ".png", result.shape[:2])
         file_helper.write_file(transkribus, path + ".xml")
 
         file_helper.write_image(result, path + ".png")
 
-        if config['Common']['semanticsegmentation'] and semantic_segmentation_image is not None:
-            file_helper.write_image(semantic_segmentation_image, path + "_semantic.png")
+        if config['Common']['semanticsegmentation'] and \
+                semantic_segmentation_image is not None:
+            file_helper.write_image(
+                semantic_segmentation_image, path + "_semantic.png")
             manifest_row['semanticsegmentation'] = image_name + '_semantic.png'
 
-        file_helper.write_annotation_file(annotations, baselines, path + ".txt")
+        file_helper.write_annotation_file(annotations, baselines, path +
+                                          ".txt")
 
         if config['Common']['annotations']:
-            result = image_helper.draw_annotations(result, annotations, baselines)
+            result = image_helper.draw_annotations(
+                result, annotations, baselines)
             file_helper.write_image(result, path + "_annotations.png")
-            manifest_row['semanticsegmentation'] = image_name + '_annotations.png'
+            manifest_row['semanticsegmentation'] = image_name + \
+                '_annotations.png'
 
         index += step
         generated += 1
@@ -211,7 +220,7 @@ def generator(config, content, index, fonts, backgrounds, args,
 
 
 class Storage:
-    """docstring for ClassName"""
+    '''Storage's purpose is to group data used further in computation'''
     def __init__(self, config, font=None):
         self.config = config
 
@@ -221,7 +230,6 @@ class Storage:
             self.fonts = {font: file_helper.load_font(font)}
         else:
             self.fonts = file_helper.load_fonts(config['Common']['fonts'])
-
 
 
 def main():
@@ -248,7 +256,6 @@ def main():
     content = file_helper.read_file(config['Common']['input'],
                                     config['Text']['words'])
     content = modify_content(content, config)
-
 
     config["FontSizes"] = {}
     config["OriginalText"] = copy.deepcopy(content)
